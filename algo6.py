bcbb1f9b6e21d4371444e9989dd19d6ea235817e
ehdqhddl <ehdqhddl91@gmail.com>
2019-12-13 00:06:07 +0900

    BFS 기초 및 실습

--- algo6.py
+++ algo6.py
@@ -14,17 +14,22 @@ graph['H'] = ['C']
 graph['I'] = ['C', 'J']
 graph['J'] = ['I']
 
-visited_queue = []
-will_visit_queue = []
 
-visited_queue.append('A')
+def bfs():
+    visited_queue = []
+    will_visit_queue = []
 
-for v in graph.values():
-    will_visit_queue += v
-    if will_visit_queue[0] not in visited_queue :
-        visited_queue.append(will_visit_queue[0])
-        del will_visit_queue[0]
-    else:
-        del will_visit_queue[0]
-print(visited_queue)
+    visited_queue.append('A')
 
+    for v in graph.values():
+        will_visit_queue += v
+    while len(will_visit_queue) != 0:
+        if will_visit_queue[0] not in visited_queue:
+            visited_queue.append(will_visit_queue[0])
+            del will_visit_queue[0]
+        else:
+            del will_visit_queue[0]
+    print(visited_queue)
+
+
+bfs()
